dd96882af52774ce222a35251d339352cd8bd903
BethCG <elisabet.capon@sdsc.ethz.ch>
2024-11-24 16:56:28 +0000

    Changed relative route

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 # ---- READ EXCEL ----
 def get_data_from_excel():
     df = pd.read_excel(
-        io="../sales-data/supermarkt_sales.xlsx",
+        io="./sales-data/supermarkt_sales.xlsx",
         engine="openpyxl",
         sheet_name="Sales",
         skiprows=3,
